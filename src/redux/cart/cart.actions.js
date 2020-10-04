7005a142dc29c99b1a9a4eea31e52a18ef0ecc9e
vandinh124 <vanvdinh124@gmail.com>
2020-10-04 13:35:02 -0700

    show items in the cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-    type: CartActionTypes.TOOGLE_CART_HIDDEN,
+    type: CartActionTypes.TOOGLE_CART_HIDDEN,    
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 })
