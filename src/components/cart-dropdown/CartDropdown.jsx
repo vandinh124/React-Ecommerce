e6e443bbd00166ca4a67d72c9e6db49dac264274
vandinh124 <vanvdinh124@gmail.com>
2020-10-04 14:31:36 -0700

    updating application with reselect

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 
 import './CartDropdown.styles.scss';
 
@@ -19,7 +20,7 @@ const CartDropdown = ({cartItems}) => (
     </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 export default connect(mapStateToProps)(CartDropdown);
