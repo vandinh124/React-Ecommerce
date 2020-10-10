6c52d25431af3966b0446ebcc00310b19ceff548
vandinh124 <vanvdinh124@gmail.com>
2020-10-09 19:03:45 -0700

    moved our directory and shop data into its own reducer

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import MenuItem from '../menu-item/MenuItem';
+import './Directory.styles.scss';
+
+const Directory = ({sections}) => (
+  <div className='directory-menu'>
+            {sections.map(({id, ...otherSectionsProps}) => (
+                <MenuItem key={id} {...otherSectionsProps}/>
+            ))}
+        </div>
+);
+  
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+})
+
+    
+
+export default connect(mapStateToProps)(Directory);
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import ShopData from './ShopData';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: ShopData
-        }
-    }
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
+import CollectionPage from '../collection/CollectionPage';
 
-    render() {
-        const {collections} = this.state;
-        return (
+const ShopPage = ({match}) => (
         <div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id}{...otherCollectionProps}></CollectionPreview>
-                ))
-            }
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
-        )
-    }
-}
+)
+
 
 
 export default ShopPage;
